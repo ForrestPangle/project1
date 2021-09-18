a8c3290405f9ce96d306b7cc9772518b4c22ff5b
ForrestPangle <87397971+ForrestPangle@users.noreply.github.com>
2021-09-18 10:08:20 -0400

    modified homepage

---
+++ assets/JavaScript/JavaScript.js
@@ -0,0 +1,28 @@
+var name = document.getElementById("name")
+var email = document.getElementById("email")
+var password = document.getElementById("password")
+var confirmPassword = document.getElementById("confirmpassword")
+
+function signUp(name,email,password,corfirmpassword){
+if (!name || name==""){alert("Please fill out all fields");
+return}
+if (!email || email==""){alert("Please fill out all fields");
+return}
+if (!password || password==""){alert("Please fill out all fields"); 
+return}
+if (!confirmPassword || confirmPassword=="" || confirmPassword.value!==password.value){alert("Make sure passwords match!")
+return}
+var users = JSON.parse(localStorage.getItem('users')) || [];
+var newUser = {
+    name: name.value,
+    email: email.value,
+    password: password.value
+    }
+    
+    users.push(newUser);
+    
+    localStorage.setItem('user',JSON.strignify(users));
+}
+
+//make onclick event to connect signup function to registration value//
+document.getElementById("registerbutton").addEventListener("click", );
