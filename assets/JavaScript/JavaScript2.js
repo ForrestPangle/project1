9822847920d7645396064d532c7659621c896ccb
Cktestakis <codyktestakis@gmail.com>
2021-09-18 10:31:51 -0400

    Little changes were made

---
+++ assets/JavaScript/JavaScript2.js
@@ -0,0 +1,8 @@
+var typeCare = document.getElementById("typeCare");
+var zipCode = document.getElementById("zipCode");
+var animalType = document.getElementById("typeAnimal");
+var enterButton = document.getElementById("enterBtn");
+var groomers = document.getElementById("groomers");
+var veterinarian = document.getElementById("veterinarian");
+
+
