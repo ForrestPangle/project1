6a53a651efeed63bca268f588ae39b84b095d000
spb71 <shem.baijoo@gmail.com>
2021-09-21 17:46:55 -0400

    Navigating to third page

--- assets/JavaScript/JavaScript2.js
+++ assets/JavaScript/JavaScript2.js
@@ -1,25 +1,29 @@
 var typeCare = document.getElementById("typeCare");
 var zipCode = document.getElementById("zipCode");
 var animalInput = document.getElementById("animal-input");
-var careInput = document.getElementById("care-input");
 var animalType = document.getElementById("typeAnimal");
 var enterButton = document.getElementById("enterBtn");
 var groomers = document.getElementById("groomers");
 var veterinarian = document.getElementById("veterinarian");
+var nextBtn = document.getElementById("nextBtn");
 
 var petAPI = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJtZ09zcGo3ZTBQcXhjbUxVM20waTg4Yzlxc1YxTTRMdFJ2a2tpU0ZDZENPYmhIZHp6QyIsImp0aSI6IjJhODRhYmEzNjEwM2YyOGNhMjgzNzIyYjc0YWM1OWZiMWY0YTg4Y2Q0OWU1OGFiN2JiZjZhMWFhNTEyOTIyNjFiMWUzMzk4M2RiYzIyYjA1IiwiaWF0IjoxNjMxOTg1MTIxLCJuYmYiOjE2MzE5ODUxMjEsImV4cCI6MTYzMTk4ODcyMSwic3ViIjoiIiwic2NvcGVzIjpbXX0.rsCGvOWzYfJyM78A1mjuQ94ftWzGV-Obrct28S_p9flFlGhh6LiQ9aPUhunk3hrdsixwiDsonwrwXJ71yb6ewMzZSkNNmllFMn_ua83FR99F40eElZQTsy81K50k4-U5OCNOtIoPcxxdbnyonJFGcGTzpZ4PJTw8S8Pfy8FVIBnTdZigx81TviHZAeNnP8Ev81P9WzPh7FdqqQV4sSNFVwLiDBRHLZ-3bCgB6wOwc31oBCb807ixzePcu7h-o67_UUMjqYXOi6Do_qgPlsdusFm1shAyNbUtz7afk2cQFXeFv0PSoKZXhjxUfWjN99ssMNtiztgeL5USsYYj1PB89Q";
 var zipAPI = "XUz25Av4wCWSh43hVoEGaLAQn0u5L6sOsDXy7HJQdOA8MV1p0ZroUKC940xmkiMF";
-
+window.localStorage.setItem("animal", animalType.value);
+window.localStorage.setItem("care", typeCare.value);
+window.localStorage.setItem("zipCode", zipCode.value);
 
 enterButton.addEventListener("click", function(e) {
     e.preventDefault();
 
-    if (animalInput == "" || careInput == "" || zipCode == "") {
-        alert("Please enter info in all fields");
-    }
+    var enteredAnimal = window.localStorage.getItem("animal");
+    var enteredCare = window.localStorage.getItem("care");
+    var enteredZipCode = window.localStorage.getItem("zipCode");
 
     petFinder();
     renderZipCode();
+    
+    window.location = "https://forrestpangle.github.io/project1/index3.html";
 })
 
 // if (animalInput.value == "" || animalInput.value == null) {
@@ -53,12 +57,13 @@ function petFinder() {
         .then(function(data) {
             console.log(data);
         })
+
     })
 }
 
 function renderZipCode() {
     var easy = zipCode.value;
-    var zipURL = "https://secret-ocean-49799.herokuapp.com/https://www.zipcodeapi.com/rest/" + zipAPI + "/radius.json/" + easy + "/10/mile";
+    var zipURL = "https://www.zipcodeapi.com/rest/" + zipAPI + "/info.json/" + easy + "/mile";
     fetch(zipURL)
     .then(function(response){
         return response.json();
